631bd528f64e1785a28f0bd4d69c555f3f32e3cb
estebanluginbuhl <estebanluginbuhl@gmail.com>
2019-05-22 14:39:33 +0200

    kujytrd

---
+++ Workshop_Arcade/Assets/Script/son/TriggerBreakTileRuby.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBreakTileRuby : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D BreakTileRuby)
+    {
+        AkSoundEngine.PostEvent("spikes_trap", this.gameObject);
+        Debug.Log("TriggerRubyTile");
+    }
+}
