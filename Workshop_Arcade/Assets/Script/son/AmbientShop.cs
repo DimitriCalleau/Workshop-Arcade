ea4ae87e5012e75fa229e4438a28216650085bdb
DimitriCalleau <dimitri.calleau@outlook.fr>
2019-05-22 17:11:57 +0200

    dsc

---
+++ Workshop_Arcade/Assets/Script/son/AmbientShop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientShop : MonoBehaviour
+{
+    public void Start()
+    {
+        AkSoundEngine.PostEvent("shop_Ambient", this.gameObject);
+    }
+}
