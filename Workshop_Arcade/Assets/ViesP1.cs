e816cf1056e7187d0394c0d77ddf317f02b7ecbf
DimitriCalleau <dimitri.calleau@outlook.fr>
2019-05-22 19:21:50 +0200

    sd

---
+++ Workshop_Arcade/Assets/ViesP1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViesP1 : MonoBehaviour
+{
+    private GameObject P1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        P1 = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Vies : " + P1.GetComponent<Character_Controller_1Player>().health)
+    }
+}
