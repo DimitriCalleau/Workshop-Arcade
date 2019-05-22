4c2085b674f95b5145fc84edc329833536d3bab4
DimitriCalleau <dimitri.calleau@outlook.fr>
2019-05-22 16:29:50 +0200

    fdfd

--- Workshop_Arcade/Assets/Script/Camera_Controller.cs
+++ Workshop_Arcade/Assets/Script/Camera_Controller.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Camera_Controller : MonoBehaviour
 {
-    public GameObject P1;
-    public GameObject P2;
+    private GameObject P1;
+    private GameObject P2;
     private float moyenne;
-    public float moyenneMax;
-    public float moyenneMin;
+    public float hauteurMax;
+    public float hauteurMin;
     private Vector3 vecteurab;
     private Vector3 vecteurMoyenne;
     private Vector3 positionP1y;
     private Vector3 positionP2y;
     public int distancCam;
-    public float oui;
+    public float trucpourlelerpjsaispascequecest;
 
     // Start is called before the first frame update
     void Start()
     {
+        P1 = GameObject.FindGameObjectsWithTag("Player")[0];
+        P2 = GameObject.FindGameObjectsWithTag("Player")[1];
         distancCam = -5;
-        moyenneMax = 17;
-        moyenneMin = -17;
+        hauteurMax = 17;
+        hauteurMin = -17;
     }
 
     // Update is called once per frame
@@ -34,7 +36,7 @@ public class Camera_Controller : MonoBehaviour
 
         vecteurMoyenne = new Vector3(0, moyenne, distancCam);
 
-        if (moyenne < moyenneMax && moyenne > moyenneMin)
+        if (moyenne < hauteurMax && moyenne > hauteurMin)
         {
             this.gameObject.transform.position = new Vector3(0, moyenne, distancCam);
         }
@@ -42,11 +44,11 @@ public class Camera_Controller : MonoBehaviour
         {
             if(P1.transform.position.y < P2.transform.position.y)
             {
-                transform.position = Vector3.Lerp(vecteurMoyenne, positionP1y, oui);
+                transform.position = Vector3.Lerp(vecteurMoyenne, positionP1y, trucpourlelerpjsaispascequecest);
             }
             if (P1.transform.position.y > P2.transform.position.y)
             {
-                transform.position = Vector3.Lerp(vecteurMoyenne, positionP2y, oui);
+                transform.position = Vector3.Lerp(vecteurMoyenne, positionP2y, trucpourlelerpjsaispascequecest);
             }
         }
     }
