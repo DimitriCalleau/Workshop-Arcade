0a19b779c416a1a592f9bfefe0d286b0d09a1dd2
DimitriCalleau <dimitri.calleau@outlook.fr>
2019-05-22 16:19:25 +0200

    dzd

--- Workshop_Arcade/Assets/Level/LevelGenerator/LevelGenerator.cs
+++ Workshop_Arcade/Assets/Level/LevelGenerator/LevelGenerator.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public int difficulty = 0;
     [SerializeField]
     static public int level = 0;
     [Space(10)]
     public int levelLenght = 10;
     [Space(10)]
     public Transform spawnPoint;
-    public GameObject player;
+    public GameObject player1;
+    public GameObject player2;
     [Space(10)]
     public Transform nextPart;
     public GameObject[] startingParts;
-    public GameObject[] levelParts;
+    public GameObject[] levelPartsEasy;
+    public GameObject[] levelPartsMedium;
+    public GameObject[] levelPartsHard;
     public GameObject[] endingParts;
 
     void Start()
     {
+        List<int> mediumPartsPos = new List<int>();
+        List<int> hardPartsPos = new List<int>();
+
+        level++;
+        for (int i = 0; i < level; i++)
+        {
+            mediumPartsPos.Add(Random.Range(0, levelLenght));
+            Debug.Log("Medium part set to index " + mediumPartsPos[i]);
+        }
+
+        for (int i = 5; i < level; i++)
+        {
+            hardPartsPos.Add(Random.Range(0, levelLenght));
+        }
+
         for (int i = 0; i < levelLenght; i++)
         {
             if (i == 0)
@@ -30,9 +47,14 @@ public class LevelGenerator : MonoBehaviour
 
                 spawnPoint = nextPart.transform.Find("SpawnPoint").transform;
 
-
-                Instantiate(player, spawnPoint.position, spawnPoint.rotation);
-
+                if (player1 != null)
+                {
+                    Instantiate(player1, spawnPoint.position, spawnPoint.rotation);
+                }
+                if (player2 != null)
+                {
+                    Instantiate(player2, spawnPoint.position, spawnPoint.rotation);
+                }
 
                 nextPart = nextPart.transform.Find("NextPart").transform;
             }
@@ -43,15 +65,36 @@ public class LevelGenerator : MonoBehaviour
             }
             else
             {
-                int partChoice = Random.Range(0, levelParts.Length);
-                nextPart = Instantiate(levelParts[partChoice], nextPart).transform.Find("NextPart").transform;
+                bool partSet = false;
+                for(int j = 0; j < mediumPartsPos.ToArray().Length; j++)
+                {
+                    if(mediumPartsPos[j] == i && !partSet)
+                    {
+                        int partChoice = Random.Range(0, levelPartsMedium.Length);
+                        nextPart = Instantiate(levelPartsMedium[partChoice], nextPart).transform.Find("NextPart").transform;
+                        partSet = true;
+                    }
+                }
+                for (int j = 0; j < hardPartsPos.ToArray().Length; j++)
+                {
+                    if (hardPartsPos[j] == i && !partSet)
+                    {
+                        int partChoice = Random.Range(0, levelPartsHard.Length);
+                        nextPart = Instantiate(levelPartsHard[partChoice], nextPart).transform.Find("NextPart").transform;
+                        partSet = true;
+                    }
+                }
+                if (!partSet)
+                {
+                    int partChoice = Random.Range(0, levelPartsEasy.Length);
+                    nextPart = Instantiate(levelPartsEasy[partChoice], nextPart).transform.Find("NextPart").transform;
+                    partSet = true;
+                }
             }
         }
-        level++;
         Debug.Log("Level " + level + " généré!");
     }
-
-    /*
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
@@ -60,5 +103,4 @@ public class LevelGenerator : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
-    */
 }
