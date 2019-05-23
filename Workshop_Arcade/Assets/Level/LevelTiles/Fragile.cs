81e3d3378baf5028517dcfe84aa98afd1479640a
nijyazz <flooster31@gmal.com>
2019-05-23 12:40:56 +0200

    rghf,h

--- Workshop_Arcade/Assets/Level/LevelTiles/Fragile.cs
+++ Workshop_Arcade/Assets/Level/LevelTiles/Fragile.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Fragile : MonoBehaviour
 {
+    public bool dropItem;
+    public GameObject gem;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag.Equals("Bullet"))
         {
+            if (dropItem && gem != null)
+            {
+                Instantiate(gem, transform.position, transform.rotation);
+            }
             Destroy(gameObject, 0.01f);
         }
     }
