66abe198814878fe5694315ecd7518cdc42966e2
DimitriCalleau <dimitri.calleau@outlook.fr>
2019-05-23 14:05:38 +0200

    "bugfix"

--- Workshop_Arcade/Assets/Script/UIManager.cs
+++ Workshop_Arcade/Assets/Script/UIManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject panelMort;
+
     private GameObject P1;
     private GameObject P2;
 
@@ -12,10 +14,13 @@ public class UIManager : MonoBehaviour
     public Text vieJoueur2;
     public Text scoreJoueur1;
     public Text scoreJoueur2;
+    public Text mort;
 
     // Start is called before the first frame update
     void Start()
     {
+        panelMort.SetActive(false);
+
         P1 = GameObject.FindGameObjectsWithTag("Player")[0];
         P2 = GameObject.FindGameObjectsWithTag("Player")[1];
     }
@@ -27,5 +32,24 @@ public class UIManager : MonoBehaviour
         vieJoueur2.text = "Vie P2: " + P2.GetComponent<Character_Controller_2Player>().health;
         vieJoueur1.text = "Vie P1: " + P1.GetComponent<Character_Controller_1Player>().health;
         scoreJoueur1.text = "Score P1: " + P1.GetComponent<Character_Controller_1Player>().score;
+
+        if (P1.GetComponent<Character_Controller_1Player>().score <= 0 || P2.GetComponent<Character_Controller_2Player>().score <= 0)
+        {
+            panelMort.SetActive(true);
+            Time.timeScale = 0;
+
+            if (P1.GetComponent<Character_Controller_1Player>().score <= 0)
+            {
+                mort.text = "Le Joueur 1 à Perdu";
+            }
+            else if (P2.GetComponent<Character_Controller_2Player>().score <= 0)
+            {
+                mort.text = "Le Joueur 2 à Perdu";
+            }
+            else
+            {
+                mort.text = "";
+            }
+        }
     }
 }
